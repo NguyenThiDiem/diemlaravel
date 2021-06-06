d55a0c05b6db80c6037388b94145e660195414cf
NguyenThiDiem <diem.nguyen22@student.passerellesnumeriques.org>
2021-06-06 14:47:16 +0700

    up to git

---
+++ resources/views/header.blade.php
@@ -0,0 +1,214 @@
+<div id="header">
+    <div class="header-top">
+        <div class="container">
+            <div class="pull-left auto-width-left">
+                <ul class="top-menu menu-beta l-inline">
+                    <li><a href=""><i class="fa fa-home"></i> 101B Lê Hưu Trác Phước Mỹ </a></li>
+                    <li><a href=""><i class="fa fa-sitemap"></i> Sitemap</a></li>
+                </ul>
+            </div>
+            <div class="pull-right auto-width-right">
+                <ul class="top-details menu-beta l-inline">
+                    <li><a href="#"><i class="fa fa-user"></i> Your Account</a></li>
+                    <li class="hidden-xs">
+                        <select name="languages">
+                            <option value="en">English</option>
+                            <option value="ro">Romana</option>
+                            <option value="ru">Rusa</option>
+                        </select>
+                    </li>
+                    <li class="hidden-xs">
+                        <select name="currency">
+                            <option value="usd">USD</option>
+                            <option value="eur">EUR</option>
+                            <option value="ron">RON</option>
+                        </select>
+                    </li>
+                </ul>
+            </div>
+            <div class="clearfix"></div>
+        </div> <!-- .container -->
+    </div> <!-- .header-top -->
+    <div class="header-body">
+        <div class="container beta-relative">
+            <div class="pull-left">
+                <a href="index.html" id="logo"><img src="source/assets/dest/images/logo-cake.png" width="200px" alt=""></a>
+            </div>
+            <div class="pull-right beta-components space-left ov">
+                <div class="space10">&nbsp;</div>
+                <div class="beta-comp">
+                    <form role="search" method="get" id="searchform" action="/">
+                        <input type="text" value="" name="s" id="s" placeholder="Search entire store here..." />
+                        <button class="fa fa-search" type="submit" id="searchsubmit"></button>
+                    </form>
+                </div>
+
+                <div class="beta-comp">
+                    <div class="cart">
+                        <div class="beta-select"><i class="fa fa-shopping-cart"></i> Cart (empty) <i class="fa fa-chevron-down"></i></div>
+                        <div class="beta-dropdown cart-body">
+                            <div class="cart-item">
+                                <a class="cart-item-edit" href="#"><i class="fa fa-pencil"></i></a>
+                                <a class="cart-item-delete" href="#"><i class="fa fa-times"></i></a>
+                                <div class="media">
+                                    <a class="pull-left" href="#"><img src="source/assets/dest/images/products/cart/1.png" alt=""></a>
+                                    <div class="media-body">
+                                        <span class="cart-item-title">Sample Woman Top</span>
+                                        <span class="cart-item-options">Size: XS; Colar: Navy</span>
+                                        <span class="cart-item-amount">1*<span>$49.50</span></span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="cart-item">
+                                <a class="cart-item-edit" href="#"><i class="fa fa-pencil"></i></a>
+                                <a class="cart-item-delete" href="#"><i class="fa fa-times"></i></a>
+                                <div class="media">
+                                    <a class="pull-left" href="#"><img src="source/assets/dest/images/products/cart/2.png" alt=""></a>
+                                    <div class="media-body">
+                                        <span class="cart-item-title">Sample Woman Top</span>
+                                        <span class="cart-item-options">Size: XS; Colar: Navy</span>
+                                        <span class="cart-item-amount">1*<span>$49.50</span></span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="cart-item">
+                                <a class="cart-item-edit" href="#"><i class="fa fa-pencil"></i></a>
+                                <a class="cart-item-delete" href="#"><i class="fa fa-times"></i></a>
+                                <div class="media">
+                                    <a class="pull-left" href="#"><img src="source/assets/dest/images/products/cart/3.png" alt=""></a>
+                                    <div class="media-body">
+                                        <span class="cart-item-title">Sample Woman Top</span>
+                                        <span class="cart-item-options">Size: XS; Colar: Navy</span>
+                                        <span class="cart-item-amount">1*<span>$49.50</span></span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="cart-caption">
+                                <div class="cart-total text-right">Subtotal: <span class="cart-total-value">$34.55</span></div>
+                                <div class="clearfix"></div>
+
+                                <div class="center">
+                                    <div class="space10">&nbsp;</div>
+                                    <a href="#" class="beta-btn primary text-center">Checkout <i class="fa fa-chevron-right"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div> <!-- .cart -->
+                </div>
+            </div>
+            <div class="clearfix"></div>
+        </div> <!-- .container -->
+    </div> <!-- .header-body -->
+    <div class="header-bottom">
+        <div class="container">
+            <a class="visible-xs beta-menu-toggle pull-right" href="#"><span class='beta-menu-toggle-text'>Menu</span> <i class="fa fa-bars"></i></a>
+            <div class="visible-xs clearfix"></div>
+            <nav class="main-menu">
+                <ul class="l-inline ov">
+                    <li><a href="index.html">Trang chủ</a>
+                        <ul class="sub-menu">
+                            <li><a href="home_10.html">Home Portfolio</a></li>
+                            <li><a href="home_4.html">Company Intro 1</a></li>
+                            <li><a href="home_5.html">Company Intro 2</a></li>
+                            <li><a href="home_7.html">Company Intro 3</a></li>
+                            <li><a href="home_6.html">Home Classic 1</a></li>
+                            <li><a href="home_8.html">Home Classic 2</a></li>
+                            <li><a href="home_9.html">Home Classic 3</a></li>
+                            <li><a href="index.html">Home Shop 1</a></li>
+                            <li><a href="home_2.html">Home Shop 2</a></li>
+                            <li><a href="home_3.html">Home Shop 3</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Pages</a>
+                        <ul class="sub-menu">
+                            <!-- <li><a href="product_type.html">Sản phẩm 1</a></li>
+                            <li><a href="product_type.html">Sản phẩm 2</a></li>
+                            <li><a href="product_type.html">Sản phẩm 3</a></li>
+                            <li><a href="product_type.html">Sản phẩm 4</a></li>
+                            <li><a href="product_type.html">Sản phẩm 5</a></li>
+                            <li><a href="product_type.html">Sản phẩm 6</a></li>
+                            <li><a href="product_type.html">Sản phẩm 7</a></li>
+                            <li><a href="product_type.html">Sản phẩm 8</a></li> -->
+                            @foreach($loai_sp as $loai)
+                                <li><a href="{{route('loaisanpham')}}">{{$loai->name}}</a></i>
+                            @endforeach;
+                            
+                        </ul>
+                    </li>
+                    <li><a href="{{route('about')}}">Giới thiệu</a></li>
+                    <li><a href="{{route('lienhe')}}">Liên hệ</a></li>
+                    <li><a href="#">Portfolio</a>
+                        <ul class="sub-menu">
+                            <li><a href="portfolio_type_a.html">Portfolio type A</a></li>
+                            <li><a href="#">Portfolio type B</a>
+                                <ul class="sub-menu">
+                                    
+                                    <li><a href="portfolio_3col.html">Portfolio B - 3 Columns</a></li>
+                                    <li><a href="portfolio_4col.html">Portfolio B - 4 Columns</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="portfolio_single.html">Portfolio Item</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Blog</a>
+                        <ul class="sub-menu">
+                            <li><a href="blog_fullwidth_1col.html">Blog Full Width</a>
+                                <ul class="sub-menu">
+                                    <li><a href="blog_fullwidth_2col.html">Blog Full Width - 2 Columns</a></li>
+                                    <li><a href="blog_fullwidth_3col.html">Blog Full Width - 3 Columns</a></li>
+                                    <li><a href="blog_fullwidth_4col.html">Blog Full Width - 4 Columns</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Blog Type A</a>
+                                <ul class="sub-menu">
+                                    <li><a href="blog_with_sidebarleft_type_a.html">Blog A - Left Sidebar</a></li>
+                                    <li><a href="blog_with_sidebarright_type_a.html">Blog A - Right Sidebar</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="blog_with_sidebarleft_type_b.html">Blog Type B</a></li>
+                            <li><a href="#">Blog Type C</a>
+                                <ul class="sub-menu">
+                                    <li><a href="blog_with_sidebarleft_type_c.html">Blog C - Left Sidebar</a></li>
+                                    <li><a href="blog_with_sidebarleft_type_c_2cols.html">Blog C - 2 Columns</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="blog_with_sidebarleft_type_d.html">Blog Type D</a></li>
+                            <li><a href="#">Blog Type E</a>
+                                <ul class="sub-menu">
+                                    <li><a href="blog_with_sidebarleft_type_e.html">Blog E - Left Sidebar</a></li>
+                                    <li><a href="blog_with_2sidebars_type_e.html">Blog E - 2 Sidebars</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Blog Single Post</a>
+                                <ul class="sub-menu">
+                                    <li><a href="single_type_image.html">Single Post Image</a></li>
+                                    <li><a href="single_type_gallery.html">Single Post Gallery</a></li>
+                                    <li><a href="single_type_video.html">Single Post Video</a></li>
+                                    <li><a href="single_type_audio.html">Single Post Audio</a></li>
+                                    <li><a href="single_type_slideshow.html">Single Post Slideshow</a></li>
+                                    <li><a href="single_type_quote.html">Single Post Quote</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="index.html">Shop</a>
+                        <ul class="sub-menu">
+                            <li><a href="index.html">Home Shop 1</a></li>
+                            <li><a href="home_2.html">Home Shop 2</a></li>
+                            <li><a href="home_3.html">Home Shop 3</a></li>
+                            <li><a href="checkout.html">Checkout</a></li>
+                            <li><a href="pricing.html">Pricing</a></li>
+                            <li><a href="shopping_cart.html">Shopping Cart</a></li>
+                            <li><a href="product.html">Product</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="contacts.html">Contact</a></li>
+                </ul>
+                <div class="clearfix"></div>
+            </nav>
+        </div> <!-- .container -->
+    </div> <!-- .header-bottom -->
+</div> <!-- #header -->
---
+++ routes/web.php
@@ -0,0 +1,103 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/vd','App\Http\Controllers\DiemController@viDu');
+
+Route::get('/test','App\Http\Controllers\DiemController@index');
+
+Route:: group(['prefix'=>'test'],function(){
+    Route::get('u1',function(){
+        echo "Chao 1";
+    });
+    Route::get('u2',function(){
+        echo "Chao 2";
+    });
+});
+
+Route::get('tinhtong', function () {
+    return view('sum');
+});
+
+Route::post('tinhtong','App\Http\Controllers\SumController@tinhTong');
+
+//Dien tich hinh tam giac vaf tu giac
+Route::get('/areaOfShape','App\Http\Controllers\AreaOfShapeController@computerArea');
+Route::post('/areaOfShape','App\Http\Controllers\AreaOfShapeController@computerArea');
+
+//Tao form va xuat du lieu ra form
+Route::get('/form','App\Http\Controllers\signupController@index');
+Route::post('form','App\Http\Controllers\signupController@displayInfor');
+
+//
+Route::get('/formArray','App\Http\Controllers\WatchController@dataWatch');
+Route::post('formArray','App\Http\Controllers\WatchController@dataWatch');
+
+
+// Route::get ('index',[
+//         'as'=>'trang-chu',
+//         'user'=>'PageController@getIndex'
+// ]);
+//
+//Route::get('/index','App\Http\Controllers\PageController@getIndex');
+//cat layout
+
+//--------------------------------------------------------
+
+Route::get('/',function(){
+    return view('welcome');
+});
+    
+Route::get('index',[
+    'as'=>'trang-chu',
+    'uses' =>'App\Http\Controllers\PageController@getIndex'
+]);
+Route::get('loai-san-pham',[
+    'as'=>'loaisanpham',
+    'uses' =>'App\Http\Controllers\PageController@getLoaiSp'
+]);
+Route::get('chi-tiet-san-pham',[
+    'as'=>'chitietsanpham',
+    'uses' =>'App\Http\Controllers\PageController@getChitiet'
+]);
+
+
+Route::get('index',[
+    'as'=>'trangchu',
+    'uses' =>'App\Http\Controllers\PageController@getIndex'
+]);
+Route::get('gioi_thieu',[
+    'as'=>'about',
+    'uses'=>'PageController@getAbout'
+]);
+Route::get('lien_he',[
+    'as'=>'lienhe',
+    'uses'=>'PageController@getLienhe'
+]);
+
+Route::get('loai-san-pham/{type}',[
+    'as'=>'loaisanpham',
+    'uses' =>'App\Http\Controllers\PageController@getLoaiSp'
+]);
+Route::get('chi-tiet-san-pham/{id}',[
+    'as'=>'chitietsanpham',
+    'uses' =>'App\Http\Controllers\PageController@getChitiet'
+]);
